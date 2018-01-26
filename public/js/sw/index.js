eb1cc1a6124983230fbbce4f488adb16984b20d2
Jordan D. Cousins <32882218+JordanCousins@users.noreply.github.com>
2018-01-26 11:28:57 -0500

    Hijacked with Custom HTML Response
    Hijacked all page requests with a custom HTML response.

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,8 @@
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    new Response('Whale hello, world!', {
-      headers: {'foo': 'bar'}
+	//Hijacking to respond to all requests with a custom html response
+	event.respondWith(
+    new Response('<h1 class="a-winner-is-me">Whale hello there, world!</h1>', {
+      headers: {'Content-Type': 'text/html'}  //allows html response in browser
     })
   );
 });
