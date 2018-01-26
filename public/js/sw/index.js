580f514e58f1ebab244ce01b37035a3c0ffb3a98
Jordan D. Cousins <32882218+JordanCousins@users.noreply.github.com>
2018-01-26 12:49:34 -0500

    Added 404 Gif Response
    Added a 404-error.gif response to all requests for non-exsisting pages.  Also provided a custom text response for any total/connection failure.

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,9 +1,14 @@
 self.addEventListener('fetch', function(event) {
-	/*  Hijacking all requests ending in ".jpg"
-  to respond with a custom .gif response  */
-  if (event.request.url.endsWith('.jpg')) { //when request ends in '.jpg'
-    event.respondWith( //respond with
-      fetch('/imgs/whale-hello.gif') //whale meme ('.gif')
-    );
-  }
+  event.respondWith(
+    /*  Respond to all requests for non-existing
+    pages with a 404 '.gif' response  */
+    fetch(event.request).then(function(response) {
+      if (response.status === 404) {  //if the requested page is invalid
+        return fetch('/imgs/404-error.gif');  //return 404 error
+      }
+      return response;
+    }).catch(function() {  //respond to all total or connection failures
+      return new Response("Oh no, that totally failed!");  //with this custom response
+    })
+  );
 });
